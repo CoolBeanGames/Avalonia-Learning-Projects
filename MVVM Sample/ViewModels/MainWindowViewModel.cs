713b62984fbd9c79b82079b2ab4a661b2f6fa43b
CoolBeanGames <callmehaptic@gmail.com>
2026-01-02 14:59:20 -0600

    implimented basic view models for MVVM

--- MVVM Sample/ViewModels/MainWindowViewModel.cs
+++ MVVM Sample/ViewModels/MainWindowViewModel.cs
@@ -3,5 +3,6 @@
     public partial class MainWindowViewModel : ViewModelBase
     {
         public string Greeting { get; } = "Welcome to Avalonia!";
+        public SimpleViewModel SimpleVM { get; } = new SimpleViewModel();
     }
 }
---
+++ MVVM Sample/ViewModels/SimpleViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Sample.ViewModels
+{
+    public class SimpleViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private string _name;
+
+        public String? Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    RaisePropertyChanged();
+                    RaisePropertyChanged(nameof(Greeting));
+                }
+            }
+
+        }
+
+        public String? Greeting
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(Name))
+                {
+                    return "Hello, World from Avalonia Samples";
+                }
+                else
+                {
+                    return $"Hello, {Name}!";
+                }
+            }
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
