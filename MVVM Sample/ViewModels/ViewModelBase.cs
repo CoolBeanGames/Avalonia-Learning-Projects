7a14613dde58e626f6a45ba802f5e8049f7c65c2
CoolBeanGames <callmehaptic@gmail.com>
2026-01-02 14:51:13 -0600

    Created MVVM project to learn more about that

---
+++ MVVM Sample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MVVM_Sample.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ MVVM Sample/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MVVM_Sample.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
